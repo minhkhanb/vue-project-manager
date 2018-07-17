e09a072940fa0f4447d5e0f530a13d939e08cf52
Robert Marriott <marriott@telus.net>
2018-07-17 15:55:03 -0700

    Implement a Vuex to LocalStorage caching mechanism

--- src/main.js
+++ src/main.js
@@ -15,5 +15,8 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  beforeCreate() {
+		this.$store.commit('initialiseStore');
+	}
 })
--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,28 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export default store = new Vuex.Store({
   state: {
 
   }
+  mutations: {
+    // See: https://www.mikestreety.co.uk/blog/vue-js-using-localstorage-with-the-vuex-store
+    initStore(state) {
+      if (localStorage.getItem('store')) {
+        this.replaceState(
+          Object.assign(state, JSON.parse(localStorage.getItem('store')))
+        )
+      }
+    }
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+})
+
+store.subscribe((mutation, state) => {
+  localStorage.setItem('store', JSON.stringify(state))
 })
